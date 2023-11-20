e7f403389b8a4c2f1849eb1634840f375ee7f3f2
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-11-20 12:52:49 +0200

    initial config

---
+++ src/main/java/lux/fm/bookingservice/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package lux.fm.bookingservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String isHealthy() {
+        return "OK";
+    }
+}
---
+++ src/test/java/lux/fm/bookingservice/AccommodationBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lux.fm.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
