74f146ebf75d7e42bee460f9bc609c804d1b53a5
Vyacheslav <torbin.official@gmail.com>
2023-11-20 14:13:16 +0200

    Added support of telegram notifications

---
+++ src/main/java/lux/fm/bookingservice/notifications/Bot.java
@@ -0,0 +1,60 @@
+package lux.fm.bookingservice.notifications;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Getter
+public class Bot extends TelegramLongPollingBot {
+    @Value("${bot.username}")
+    private String botUsername;
+    @Value("${bot.token}")
+    private String botToken = "";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            String text = update.getMessage().getText();
+            String chatId = update.getMessage().getChatId().toString();
+            SendMessage sendMessage = new SendMessage();
+            if (text.startsWith("/test")) {
+                sendMessage.setText("OK");
+                sendMessage.setChatId(chatId);
+            } else {
+                sendMessage.setText("Command is not in list");
+                sendMessage.setChatId(chatId);
+            }
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Sending message for all users in db
+     *
+     * @param text text of message
+     */
+
+    public void setSendMessageToAll(String text) {
+        //TODO: send message for ll users
+    }
+
+    /**
+     * Sending message to user with specified id
+     *
+     * @param text text of message
+     * @param id   users id in telegram
+     */
+
+    public void sendMessageToUser(String text, Long id) {
+        //TODO:
+    }
+}
