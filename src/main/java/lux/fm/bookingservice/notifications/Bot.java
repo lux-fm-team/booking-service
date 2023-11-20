c806c430130ac79b700902f64140fa2246f3d316
Vyacheslav <torbin.official@gmail.com>
2023-11-20 17:06:22 +0200

    Syntax fixes

--- src/main/java/lux/fm/bookingservice/notifications/Bot.java
+++ src/main/java/lux/fm/bookingservice/notifications/Bot.java
@@ -9,9 +9,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 @Getter
 @RequiredArgsConstructor
--- src/main/java/lux/fm/bookingservice/service/NotificationService.java
+++ src/main/java/lux/fm/bookingservice/service/NotificationService.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface NotificationService {
     void notifyUser(/*TODO: add User as param*/String message);
+
     void notifyAllUsers(String message);
 }
