05a7232567d602459b6ccd3a4318a1a695f8c17e
Parashchuk <Andry.parashchuk@gmail.com>
2023-11-21 10:29:12 +0100

    Implement lifeTime, liquibase changeset

--- src/main/java/lux/fm/bookingservice/model/Booking.java
+++ src/main/java/lux/fm/bookingservice/model/Booking.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,4 +37,7 @@ public class Booking {
 
     @Column(name = "check_out", nullable = false)
     private LocalDate checkOut;
+
+    @Column(name = "life_time", nullable = false)
+    private LocalTime lifeTime = LocalTime.now().plusMinutes(20);
 }
