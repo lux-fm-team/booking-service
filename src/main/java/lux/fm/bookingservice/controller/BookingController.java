e7dafdf2c30289528743374bf863599ec97597b0
Parashchuk <Andry.parashchuk@gmail.com>
2023-11-21 10:56:28 +0100

    raw

--- src/main/java/lux/fm/bookingservice/controller/BookingController.java
+++ src/main/java/lux/fm/bookingservice/controller/BookingController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -68,4 +69,7 @@ public class BookingController {
     ) {
         return bookingService.findBookingById(authentication.getName(), id);
     }
+
+    @PutMapping
+    public BookingResponseDto updateBookingById()
 }
