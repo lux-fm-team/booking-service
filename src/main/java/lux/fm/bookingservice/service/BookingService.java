26880022914e9e7519aa13d6c8cf9118d90d7cfc
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-21 14:56:14 +0200

    checkstyle fixes

--- src/main/java/lux/fm/bookingservice/service/BookingService.java
+++ src/main/java/lux/fm/bookingservice/service/BookingService.java
@@ -12,6 +12,7 @@ public interface BookingService {
     List<BookingResponseDto> findMyBookings(String token);
 
     BookingResponseDto findBookingById(String token, Long id);
+
     BookingResponseDto addBooking(BookingRequestDto request);
 
     BookingResponseDto updateBookingById(BookingRequestUpdateDto requestUpdateDto, Long id);
