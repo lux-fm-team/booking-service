d7f8585f5e6280f13a1669797df3a29019ba0ae8
Alex Rosenkreuz <ososkdkc@gmail.com>
2023-11-21 04:53:26 +0200

    Deleted unused imports

--- src/main/java/lux/fm/bookingservice/dto/user/UserResponseDtoWithRoles.java
+++ src/main/java/lux/fm/bookingservice/dto/user/UserResponseDtoWithRoles.java
@@ -2,8 +2,6 @@ package lux.fm.bookingservice.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Set;
 import lombok.Getter;
