816a539071d550cd27b117a374b5d01c3b901227
Vyacheslav <torbin.official@gmail.com>
2023-11-21 17:51:53 +0200

    Typo fixes. Added notifications for booking service

--- src/main/java/lux/fm/bookingservice/service/NotificationService.java
+++ src/main/java/lux/fm/bookingservice/service/NotificationService.java
@@ -1,10 +1,9 @@
 package lux.fm.bookingservice.service;
 
-import lux.fm.bookingservice.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface NotificationService {
-    void notifyUser(User user, String message);
+    void notifyUser(Long id, String message);
 
 }
--- src/main/java/lux/fm/bookingservice/service/impl/TelegramNotificationService.java
+++ src/main/java/lux/fm/bookingservice/service/impl/TelegramNotificationService.java
@@ -1,7 +1,6 @@
 package lux.fm.bookingservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import lux.fm.bookingservice.model.User;
 import lux.fm.bookingservice.notifications.Bot;
 import lux.fm.bookingservice.service.NotificationService;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,7 @@ public class TelegramNotificationService implements NotificationService {
     private final Bot bot;
 
     @Override
-    public void notifyUser(User user, String message) {
-        bot.sendMessageToUser(message, user.getTelegramId());
+    public void notifyUser(Long id, String message) {
+        bot.sendMessageToUser(message, id);
     }
 }
