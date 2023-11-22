e247be96cd56764612108ebc36dbec6d2ff14883
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-22 18:04:11 +0200

    [test] added tests for AuthenticationController, creadted testContainer

---
+++ src/test/resources/database/users/add-user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, email, first_name, last_name , password , is_deleted)
+VALUES (1, 'test@check.com', 'testFirstName', 'testLastName' , '$2a$10$Qzz79ycwjsShQuaQAF646.p23RnVjfH7pkimLIT7uPldlQkt8vIp2' , false);
