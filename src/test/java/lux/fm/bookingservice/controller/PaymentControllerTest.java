b4ff5cc297bde186624ddaf2e1e746e34d11fa2f
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-23 23:23:29 +0200

    [test] removed testcontainers

--- src/test/java/lux/fm/bookingservice/controller/PaymentControllerTest.java
+++ src/test/java/lux/fm/bookingservice/controller/PaymentControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
@@ -30,11 +31,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "classpath:database/payments/add-all-entities.sql")
 @Sql(scripts = "classpath:database/payments/delete-all-entities.sql",
         executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
-public class PaymentControllerTest extends AbstractPostgresAwareTest {
+public class PaymentControllerTest  {
     protected static MockMvc mockMvc;
     private static ObjectMapper objectMapper;
 
