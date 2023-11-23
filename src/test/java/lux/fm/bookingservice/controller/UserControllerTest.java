5e98507264f5c82a36cacff3f77cb67b8c90e52e
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-11-24 00:45:18 +0200

    [fixes] user tests

--- src/test/java/lux/fm/bookingservice/controller/UserControllerTest.java
+++ src/test/java/lux/fm/bookingservice/controller/UserControllerTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
@@ -29,10 +28,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Sql(scripts = "classpath:database/users/add-user.sql")
 @Sql(scripts = {
         "classpath:database/users/delete-user.sql"},
