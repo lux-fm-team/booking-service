2eba3e0220e511638d0964f6c210e3b43cc054f6
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-23 23:29:55 +0200

    [test] added fixes

--- src/test/java/lux/fm/bookingservice/controller/PaymentControllerTest.java
+++ src/test/java/lux/fm/bookingservice/controller/PaymentControllerTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
@@ -29,6 +30,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "classpath:database/payments/add-all-entities.sql")
 @Sql(scripts = "classpath:database/payments/delete-all-entities.sql",
         executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
