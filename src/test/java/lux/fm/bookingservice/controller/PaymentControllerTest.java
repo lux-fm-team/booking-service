a5c8853a51e540a674709881a4f426196f522408
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-23 20:30:48 +0200

    [test] check commit

---
+++ src/test/java/lux/fm/bookingservice/controller/PaymentControllerTest.java
@@ -0,0 +1,57 @@
+package lux.fm.bookingservice.controller;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.SneakyThrows;
+import lux.fm.bookingservice.dto.payment.CreatePaymentRequestDto;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlMergeMode;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "classpath:database/users/delete-all-entities.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+public class PaymentControllerTest {
+    protected static MockMvc mockMvc;
+    private static ObjectMapper objectMapper;
+
+    @BeforeAll
+    @SneakyThrows
+    static void beforeAll(@Autowired WebApplicationContext applicationContext) {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+        objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+    @Sql(scripts = "classpath:database/users/add-all-entities.sql")
+    @Test
+    @DisplayName("creating payment session")
+    @WithMockUser(username = "test@check.com", roles = {"CUSTOMER"})
+    void createPaymentSession_WithValidData_ShouldReturn() throws Exception {
+        CreatePaymentRequestDto requestDto = new CreatePaymentRequestDto(
+                1L
+        );
+        MvcResult mvcResult = mockMvc.perform(post("/api/payments")
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+}
